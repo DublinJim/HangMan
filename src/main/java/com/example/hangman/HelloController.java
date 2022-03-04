b2d37b10faf82480448d9aba6c8bb49231645d96
James Keogh <jkeogh@adga.ca>
2022-03-04 11:31:37 -0500

    adds a hit box counter and sets a found flag

--- src/main/java/com/example/hangman/HelloController.java
+++ src/main/java/com/example/hangman/HelloController.java
@@ -12,49 +12,82 @@ import javafx.scene.text.Text;
 
 public class HelloController implements Initializable {
 
+    boolean foundLetter;
+    int hits = 0;
+    int maxHits = 5;
     @FXML
     private TextField guessBoxInput;
-
     @FXML
     private Text testOutTxt;
     @FXML
     private Text instructionText1;
-
+    @FXML
+    private Text hitBox2;
+    @FXML
+    private Text hitsBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instructionText1.setText(" Begin - Enter your letter");
+
+        guessBoxInput.setPrefWidth(30);
+        guessBoxInput.setMaxWidth(80);
+        guessBoxInput.clear();
+        hitsBox.setText(String.valueOf(0));
+        hitBox2.setText(String.valueOf(maxHits));
     }
 
     @FXML
     void grabAnswer() {
-        int hits = 0;
 
+        hitBox2.setText(String.valueOf(maxHits));
         ArrayList<Character> guessesList = new ArrayList<>();
+
         String wordToGuess = "CAT";
+
+        //get a single char from the user
+        System.out.println(" input box size before = " + guessBoxInput.getLength());
         char guessFromUser = guessBoxInput.getCharacters().charAt(0);
+        System.out.println(" input box size = " + guessBoxInput.getLength());
+        //convert the word to guess to an array of chars
         char[] astrikGuess = wordToGuess.toCharArray();
+
+        //add a char to get a count (possible remove)
         guessesList.add('.');
 
         testOutTxt.setText(String.valueOf(guessFromUser));
 
-        while (hits <= 5) {
+        while (hits <= maxHits) {
             instructionText1.setText("Enter a letter");
-
             guessFromUser = guessBoxInput.getCharacters().charAt(0);
             guessFromUser = Character.toUpperCase(guessFromUser);
             String toDisplayGuessChar = String.valueOf(guessFromUser);
-            instructionText1.setText("You chose " + toDisplayGuessChar);
+            testOutTxt.setText("You chose " + toDisplayGuessChar);
 
-            for (int i = 0; i <guessesList.size() ; i++) {
+            //Check for a correct letter
+            for (int i = 0; i < astrikGuess.length; i++) {
 
+                if (guessFromUser == astrikGuess[i]) {
+                    foundLetter = true;
+                }
             }
-            hits++;
+            if (foundLetter) {
+                testOutTxt.setText("Hit!");
+            }else
+            {
+                testOutTxt.setText("Not found");
+            }
 
+            guessBoxInput.clear();
+            System.out.println(" input box size after clear = " + guessBoxInput.getLength());
+            guessesList.add(guessFromUser);
+
+            hits++;
+            hitsBox.setText(String.valueOf(hits));
 
         }
 
-
+        instructionText1.setText("done");
     }
 
     @FXML
