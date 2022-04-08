282b060675da06a855eb42ee8062c97d31ce1267
James Keogh <jkeogh@adga.ca>
2022-04-08 15:17:59 -0400

    reloads stylesheet

--- src/main/java/com/example/hangman/HelloApplication.java
+++ src/main/java/com/example/hangman/HelloApplication.java
@@ -8,22 +8,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-            HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+
     public static void main(String[] args) {
 
         launch();
 
     }
 
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+            HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 }
